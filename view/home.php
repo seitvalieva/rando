a63950557d9d52d8261a826534b4b9ba05ca11d3
seitvalieva <sseitvalieva@gmail.com>
2024-07-05 01:10:11 +0200

    added svg files, home page markup

---
+++ view/home.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home page</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <div>
+                <img src="../public/assets/logo-hiking-100x100.png" alt="logo">
+                <h3>Rando</h3>
+            </div>
+            <div>
+                <a href="">Publier une rando</a>
+                <a href="">Connexion</a>
+                <a href="">Inscription</a>
+            </div>
+        </nav>
+    </header>
+    <main>
+        <div class="features">
+            <div>
+                <h3>Inscrivez-vous gratuitement <br>
+                et rejoignez des randos</h3>
+                <a href="">Consulter nos randos</a>
+            </div>
+            <div>
+                <div>
+                    <form action="">
+                        <label for="search">Rechercher une rando</label>
+                        <div>
+                            <input type="search" id="search" name="search" placeholder="Ville où lieu à visiter">
+                            <input type="submit" value="Search">
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <div class="cards container">
+
+            <div>
+                
+            </div>
+            <div>
+
+            </div>
+            <div>
+
+            </div>
+        </div>
+    </main>
+    
+</body>
+</html>
--- view/layout.php
+++ view/layout.php
@@ -1,11 +1,38 @@
+<!-- le fichier "template.php" qui servira de base / squelette à l'ensemble des vues -->
+
+<!-- pour  déclarer le doctype, les links css / js etc qu'une seule fois dans ce fichier.
+
+On exploitera ce qu'on appelle "la temporisation de sortie" en PHP  -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="./public/css/<?=$lienCss?>.css">
+    <title><?= $titre ?></title>
 </head>
 <body>
-    
+    <header>
+        <nav>
+
+        </nav>
+    </header>
+
+    <main>
+        <?= $page ?>
+    </main>
+
+    <footer>
+
+    <p>
+        <a href="#">Conditions générales d’utilisation</a>  
+        <a href="#">Mentions légales</a>
+        <a href="#">Politique de confidentialité</a>
+        &copy; <?= date_create("now")->format("Y") ?>Rando LLC Tous droits réservés. 
+    </p>
+
+    </footer>
+    <script src ="./public/js/main.js"></script> 
 </body>
 </html>
