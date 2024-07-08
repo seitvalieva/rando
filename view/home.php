de72b1be345e8b70ad27a725e3d5fe2b2e9179b1
seitvalieva <sseitvalieva@gmail.com>
2024-07-08 11:48:28 +0200

    added styles for header, favicon, hover effects

--- view/home.php
+++ view/home.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../public/css/style.css">
+    <link rel="shortcut icon" href="../public/assets/favicon.png" type="image/x-icon">
     <title>Accueil</title>
 </head>
 <body>
