a3f6674579e672a9b3366aeb56479a5e49c9def5
seitvalieva <sseitvalieva@gmail.com>
2024-07-28 23:07:35 +0200

    added slideshow

--- public/js/script.js
+++ public/js/script.js
@@ -1,18 +1,46 @@
+// ================ BURGER MENU =============================//
 const handleMobileMenu = () => {
-    
-    const navBurgerMenu = document.getElementById('navBurgerMenu')
-    const mobileMenu = document.getElementById('mobileMenu')
-    const mobileMenuClose = document.getElementById('mobileMenuClose')
+  const navBurgerMenu = document.getElementById("navBurgerMenu");
+  const mobileMenu = document.getElementById("mobileMenu");
+  const mobileMenuClose = document.getElementById("mobileMenuClose");
 
-    const openMenu = () => {
-        mobileMenu.classList.add('mobile-menu--open')
-    }
-    const closeMenu = () => {
-        mobileMenu.classList.remove('mobile-menu--open')
-    }
+  const openMenu = () => {
+    mobileMenu.classList.add("mobile-menu--open");
+  };
+  const closeMenu = () => {
+    mobileMenu.classList.remove("mobile-menu--open");
+  };
 
-    navBurgerMenu.addEventListener('click', openMenu)
-    mobileMenuClose.addEventListener('click', closeMenu)
+  navBurgerMenu.addEventListener("click", openMenu);
+  mobileMenuClose.addEventListener("click", closeMenu);
+};
+
+handleMobileMenu();
+
+// ================ SLIDESHOW =============================//
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
 }
-
-handleMobileMenu()
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
