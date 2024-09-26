62c7bb5f88e750af01d1f8a1065f7c22dd634933
seitvalieva <sseitvalieva@gmail.com>
2024-09-26 15:06:21 +0200

    added condition to check if user already subscribed to the same rando

--- controller/SubscriptionController.php
+++ controller/SubscriptionController.php
@@ -21,17 +21,23 @@ public function participateForm() {
     ];
 }
 public function participate($id) {
-    // $randoId = $_GET["id"];
-    // echo $randoId; die();
+
     if(isset($_POST['submitParticipation']) && isset($_POST["agreeToRules"])) {
 
         $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
         $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_FULL_SPECIAL_CHARS, FILTER_VALIDATE_EMAIL);
         $userId = Session::getUser()->getId();
 
-        if($username && $email) {
+        $subscriptionManager = new SubscriptionManager();
+        
+        $user = $subscriptionManager->checkUserSubscribed($userId, $id);
+        // var_dump($user); die();
 
-            $subscriptionManager = new SubscriptionManager();
+        if($user) {
+            die("Already subscribed!");
+        }
+
+        if($username && $email) {
 
             $data = [
                 'user_id' => $userId,
--- model/managers/SubscriptionManager.php
+++ model/managers/SubscriptionManager.php
@@ -8,10 +8,23 @@ use App\DAO;
 
 class SubscriptionManager extends Manager{
 
-    protected $classname = "Model\Entities\Subscription";
+    protected $className = "Model\Entities\Subscription";
     protected $tableName = "subscription";
 
     public function __construct(){
         parent::connect();
+    }
+
+    public function checkUserSubscribed($userId, $id) {//Query that checks if the user exists by his email
+        $sql ="SELECT * 
+                FROM ".$this->tableName. " t
+                WHERE user_id = :userId
+                AND rando_id =:id";
+
+        // the query returns an object or nothing getOneOrNullResult (fonctions in Manager)
+        return  $this->getOneOrNullResult(
+            DAO::select($sql, ['userId' => $userId, 'id' => $id], false), //we add "false" because the public static function select in DAO returns multiple "true" responses
+            $this->className
+        );
     }
 }
