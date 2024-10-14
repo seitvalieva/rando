033cfefb78e2a511df5be0eb087788f4bc7cf169
seitvalieva <sseitvalieva@gmail.com>
2024-10-14 18:32:13 +0200

    deleted credentials

--- app/GoogleSignin.php
+++ app/GoogleSignin.php
@@ -4,8 +4,8 @@ require __DIR__ . "/../vendor/autoload.php";
 
 $client = new Google\Client;
 
-$client->setClientId("25074705049-g0ji4vqcveuufa8l2cnm6qvmpmo20c3p.apps.googleusercontent.com");
-$client->setClientSecret("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");
+$client->setClientId("");
+$client->setClientSecret("");
 $client->setRedirectUri("http://localhost/index.php?ctrl=security&action=googleSignin");
 
 $client->addScope("email");
