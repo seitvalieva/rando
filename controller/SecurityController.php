1a403efdd4cbbb79abb8e9a3d0db12b376982481
seitvalieva <sseitvalieva@gmail.com>
2024-10-25 12:02:35 +0200

    updated register function

--- controller/SecurityController.php
+++ controller/SecurityController.php
@@ -17,90 +17,107 @@ use PHPMailer\PHPMailer\Exception;
 class SecurityController extends AbstractController{
     // will contain the methods related to authentication: register, login and logout
     //setting up the REGISTER function
-    public function register(){
+    public function register() {
+        $errors = [];
+
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            // csrf token validation
+            if(isset($_POST['csrf_token']) && isset($_SESSION['csrf_token']) && hash_equals($_SESSION['csrf_token'], $_POST['csrf_token'])) {
             
-            if(isset($_POST['csrf_token']) && isset($_SESSION['csrf_token'])) {
-                if(!hash_equals($_SESSION['csrf_token'], $_POST['csrf_token'])) {
-                     // If the CSRF token is invalid, stop the request
-                    die("Invalid CSRF token.");
-                }
-            }
-            if (isset($_POST["agree"])) {
-                // reCAPTCHA validation
-                $recaptcha_secret = '6Leyol0qAAAAAImWdHDATp6U7uQDp7SmXE0hjwnn';  // Replace with your secret key from reCAPTCHA
-                $recaptcha_response = $_POST['g-recaptcha-response']; // The reCAPTCHA response from the form
-    
-                // Verify the reCAPTCHA response with Google
-                $response = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret={$recaptcha_secret}&response={$recaptcha_response}");
-                $response_keys = json_decode($response, true);
-         
-                // If the reCAPTCHA validation fails
-                if(intval($response_keys["success"]) !== 1) {
-                    Session::addFlash('error', "reCAPTCHA verification failed. Please try again.");
-                    header("Location: index.php?ctrl=security&action=register");
-                    exit;
-                }
-                //filtering registration form fields
+            //username validation
+            if (isset($_POST['username'])) {
                 $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-                $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_FULL_SPECIAL_CHARS, FILTER_VALIDATE_EMAIL);
-                $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-                $confirmPassword = filter_input(INPUT_POST, "confirmPassword", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-                //checks that username starts with letter, can contain only letters, numbers and underscore min 4 max 25 characters, doesn't end with underscore
-                if(!preg_match('/^[a-z]\w{2,23}[^_]$/i', $username)) {
-                    Session::addFlash('error',"Le nom d'utilisateur n'est pas valid.");
-                    header("Location: index.php?ctrl=security&action=register");
-                    exit;
+                if($username && preg_match('/^[a-z]\w{2,23}[^_]$/i', $username)) {
+                    $userManager = new UserManager();
+                    $usernameExists = $userManager->checkUsernameExists($username);
+                    // if username is already taken
+                    if($usernameExists) {
+                        $errors['username'] = "Le pseudo est déjà pris";
+                    }
+                } else {
+                    $errors['username'] = "Le nom d'utilisateur n'est pas valid.";
                 }
+            } else {
+                $errors['username'] = "Le champ du pseudo est requis.";
+            }
+            // Email validation
+            if (isset($_POST['email']))  {
+                $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_FULL_SPECIAL_CHARS, FILTER_VALIDATE_EMAIL);
+                if($email){
+                    $userManager = new UserManager();
+                    $user = $userManager->checkUserExists($email);
+                    // if user exists in db
+                    if($user) {
+                        $errors['email'] = "L'email est déjà pris";
+                    }
+                } else {
+                    $errors['email'] = "L'email n'est pas valid.";
+                }
+            } else {
+                    $errors['email'] = "Le champ du email est requis.";
+            }
+            // Password validation
+            if (isset($_POST['password'])) {
+                $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
                 // checking password requirements
                 $pattern = '/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{12,}$/';
-                if (! preg_match($pattern, $password)) {
-                    Session::addFlash('error',"Le mot de passe doit contenir au moins une lettre, un chiffre, un caractère spécial et comporter au moins 8 caractères");
-                    header("Location: index.php?ctrl=security&action=register");
-                    exit;
-                    // die("Password must contain at least one letter, one number, one special symbol and be at least 8 characters long");
-                }
-                //check filter validity
-                if($username && $email && $password && $confirmPassword){
-                    // var_dump("ok");die;
-                    $userManager = new UserManager();
-                    //function to check if the user with the same email exists
-                    $user = $userManager->checkUserExists($email);
-                    $usernameExists = $userManager->checkUsernameExists($username);
-                    //if the user is connected
-                    if($user || $usernameExists){
-                        Session::addFlash('error',"Le mail ou pseudo est déjà pris");
-                        header("Location: index.php?ctrl=security&action=register"); 
-                        exit; 
-                    } else {
-                        
-                        //inserting the user into the database
-                        if($password == $confirmPassword && strlen($password) >= 12) {//verification that passwords are identical
-                            //we retrieve the add function from the Manager file
-                            $userManager->add([ 
-                                "username" => $username,
-                                "email" => $email,
-                                "password" => password_hash($password, PASSWORD_DEFAULT)
-                            ]);
-                            //redirection after the registration
-                            header("Location: index.php?ctrl=security&action=login");
-                            exit;
-                        } else {
-                            Session::addFlash('error',"Les mots de passe ne sont pas identique");
-                            header("Location: index.php?ctrl=security&action=register");
-                            exit;
-                        }
-                    }
-                }
+
+                if(!$password || !preg_match($pattern, $password)) {
+
+                    $errors['password'] = "Le mot de passe doit contenir au moins une lettre, un chiffre, 
+                    un caractère spécial et comporter au moins 12 caractères";
+                } 
+            } else {
+                $errors['password'] = "Le champ mot de passe est requis.";
             }
+            // Confirm Password validation
+            if (isset($_POST['confirmPassword'])) {
+                $confirmPassword = filter_input(INPUT_POST, "confirmPassword", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+                if($password !== $confirmPassword) {
+                    $errors['confirmPassword'] = "Les mots de passe doivent être identiques.";
+                }
+            } else {
+                $errors['confirmPassword'] = "Veuillez confirmer votre mot de passe.";
+            }
+            // Checkbox validation 
+            if (!isset($_POST["agree"])) {
+                $errors['agree'] = "Veuillez accepter les conditions générales d’utilisation";
+            }
+            // ReCaptcha validation
+            $recaptcha_secret = '6Leyol0qAAAAAImWdHDATp6U7uQDp7SmXE0hjwnn';  // Replace with your secret key from reCAPTCHA
+            $recaptcha_response = $_POST['g-recaptcha-response']; // The reCAPTCHA response from the form
+            // Verify the reCAPTCHA response with Google
+            $response = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret={$recaptcha_secret}&response={$recaptcha_response}");
+            $response_keys = json_decode($response, true);
+            // If the reCAPTCHA validation fails
+            if(intval($response_keys["success"]) !== 1) {
+                $errors['recaptcha'] = "Veuillez vérifier le reCAPTCHA.";
+            }
+            } else {
+                $errors['csrf'] = "Invalid CSRF token.";
+            }   
+            if (empty($errors)) {
+                //inserting the user into the database
+                //we retrieve the add function from the Manager
+                $userManager->add([ 
+                "username" => $username,
+                "email" => $email,
+                "password" => password_hash($password, PASSWORD_DEFAULT)
+                ]);
+                //redirection after the registration
+                header("Location: index.php?ctrl=security&action=login");
+                exit;
+            } else {
+                $_SESSION['errors'] = $errors;  // Store errors in session
+            }
+            
         }
-            // check if POST method is submitted and the checkbox is checked
         return [
             "view" => VIEW_DIR."connection/register.php",
             "meta_description" => "Formulaire d'inscription",
             "title" => "Formulaire d'inscription"
         ];
-    }  
+    } 
 
     //  SETTING UP THE LOG IN FUNCTION
     public function login() {
