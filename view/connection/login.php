de3d023e8e103d30f3a7c22d93f8d98ca147c128
seitvalieva <sseitvalieva@gmail.com>
2024-11-15 15:47:05 +0100

    added login page styles

--- view/connection/login.php
+++ view/connection/login.php
@@ -1,48 +1,55 @@
 <?php
 require "app/SignInGoogle.php";
 ?>
-<div class="main__container">
+<div class="fullscreen-container">
+    <div class="login-container">
+        <h3 class="login-title">Connexion</h3>
 
-        <h1>Connexion</h1>
+        <span style="color: red">
+            <?= App\Session::getFlash("error") ?>
+        </span>
 
-        <span style="color: red"><?= App\Session::getFlash("error") ?></span>
-
-        <button
-                onclick="location.href='<?= $url ?>'"
-                style="width: 25%; height: 40px;">Se connecter avec Google</button>
-        <!-- <a href="">Se connecter avec Google</a> -->
+        <div class="button-container">
+            <button class="ggl-btn" onclick="location.href='<?= $url ?>'">Se connecter avec Google</button>
+        </div>
         <div class="break-container">
-                <div class="break-border-ou"></div>
-                <span class="break-content">ou</span>
-                <div class="break-border-ou"></div>
+            <div class="break-border-ou"></div>
+                <span class="break-content-or">ou</span>
+            <div class="break-border-ou"></div>
         </div>
 
         <form action="index.php?ctrl=security&action=login" method="POST" autocomplete="off">
-                
-                <input type="hidden" name="csrf_token" value="<?= htmlspecialchars($_SESSION['csrf_token']) ?>">
+            <input type="hidden" name="csrf_token" value="<?= htmlspecialchars($_SESSION['csrf_token']) ?>">
 
+            <div class="input-group">
                 <label for="email">Email</label>
                 <?php $email = isset($_POST['email']) ? $_POST['email']: ''?>
-                <input type="email" name="email" id="email" placeholder="Entrez votre email" value="<?= htmlspecialchars($email); ?>" required><br>
-
+                <input type="email" name="email" id="email" placeholder="ex : utilisateur@gmail.com" 
+                    value="<?= htmlspecialchars($email); ?>" required>
+            </div>
+            <div class="input-group">
                 <label for="password">Mot de passe</label>
-                <input type="password" name="password" id="password" placeholder="Entrez votre mot de passe" required><br><br>
-
-                <button type="submit" name="submitLogin" value="login" style="width: 100%; height: 40px;">Se
-                        connecter</button><br>
-
+                <input type="password" name="password" id="password" required>
+            </div>
+            <div class="button-container">
+                <button type="submit" name="submitLogin" value="login" class="login-btn">Se connecter</button>
+            </div>
         </form>
-        <!-- <a href="">Mot de passe oublié?</a> -->
-        <button type="submit" name="forgottenPassword"
-                onclick="location.href='index.php?ctrl=security&action=sendForgottenPasswordReset'"
-                style="width: 25%; height: 40px;">Mot de passe oublié?</button>
-        <div class="break-container">
-                <div class="break-border-connexion"></div>
-                <span class="break-content">Première connexion ?</span>
-                <div class="break-border-connexion"></div>
-        </div>        
-        <button type="submit" name="register"
-                onclick="location.href='index.php?ctrl=security&action=register'"
-                style="width: 25%; height: 40px;">Créer un compte</button>
 
+        <div class="button-container">
+            <a href="index.php?ctrl=security&action=sendForgottenPasswordReset">Mot de passe oublié?</a>
+        </div>
+
+        <div class="break-container">
+            <div class="break-border-connexion"></div>
+                <span class="break-content-connexion">Première connexion ?</span>
+            <div class="break-border-connexion"></div>
+        </div>
+
+        <div class="button-container">
+            <button type="submit" name="register" class="register-btn"
+                onclick="location.href='index.php?ctrl=security&action=register'">Créer un compte
+            </button>
+        </div>      
+    </div>
 </div>
